0756fb7c1efd124e8492131f9b69415ef6d45fb0
Miguel <mimartinezocho@gmail.com>
2023-11-20 09:28:12 -0600

    first commit

---
+++ RazorPageMovie/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPageMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        // El simbolo ? se utiliza para indicar que el dato puede ir nullo
+        [StringLength(60,MinimumLength =3)]
+        [Required]
+        public string? Title { get; set; }
+        // las etiquetas se utilizan para dar un formato especifico al tipo de dato solicitado
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }
+
+    }
+}
